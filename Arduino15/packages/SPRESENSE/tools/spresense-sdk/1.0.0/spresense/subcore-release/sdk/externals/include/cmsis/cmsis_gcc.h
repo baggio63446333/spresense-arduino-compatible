2eb7e5f717f012acc8f3fe8f6e971b6054bb4540
baggio63446333 <baggio63446333@users.noreply.github.com>
2021-01-20 11:33:36 +0900

    spresense-sdk: Update spresense-sdk precompiled libraries
    These libraries are created on `tensorflow` branch of below repository.
    https://github.com/baggio63446333/spresense

--- Arduino15/packages/SPRESENSE/tools/spresense-sdk/1.0.0/spresense/subcore-release/sdk/externals/include/cmsis/cmsis_gcc.h
+++ Arduino15/packages/SPRESENSE/tools/spresense-sdk/1.0.0/spresense/subcore-release/sdk/externals/include/cmsis/cmsis_gcc.h
@@ -1879,6 +1879,17 @@ __STATIC_FORCEINLINE uint32_t __SXTB16(uint32_t op1)
   return(result);
 }
 
+__STATIC_FORCEINLINE uint32_t __SXTB16_RORn(uint32_t op1, uint32_t rotate)
+{
+  uint32_t result;
+  if (__builtin_constant_p(rotate) && ((rotate == 8U) || (rotate == 16U) || (rotate == 24U))) {
+    __ASM volatile ("sxtb16 %0, %1, ROR %2" : "=r" (result) : "r" (op1), "i" (rotate) );
+  } else {
+    result = __SXTB16(__ROR(op1, rotate)) ;
+  }
+  return result;
+}
+
 __STATIC_FORCEINLINE uint32_t __SXTAB16(uint32_t op1, uint32_t op2)
 {
   uint32_t result;
